9f5aba19ce2b9e0cff93920fdb08bd6e7f7ce01f
Justin Kenney <justinkenney13@gmail.com>
2018-06-11 20:59:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -56,4 +56,10 @@ class Owner
   def play_with_cats
     pets[:cats].each {|cat| cat.mood = "happy"}
   end
+
+  def feed_fish
+    pets[:fishes].each {|fish| fish.mood = "happy"}
+    
+  end
+  
 end
